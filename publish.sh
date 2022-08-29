e10038cd8ad011b3111496b73ac274573522f791
Chandra Shekhar Rawat <cs215100@gmail.com>
2022-08-29 14:50:17 +0530

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -11,8 +11,8 @@ modified_files=( $(git diff-tree --no-commit-id --name-only -r $(git log --forma
 for i in "${modified_files=[@]}"
 do
         echo "$i"
-        newVersion=$(git diff HEAD~1 HEAD "$i" | grep "^+version" | awk '{print $2}')
-        oldVersion=$(git diff HEAD~1 HEAD "$i" | grep "^-version" | awk '{print $2}')
+        newVersion=$(git diff $(git log --format="%H" -n 2 | tail -1) HEAD "$i" | grep "^+version" | awk '{print $2}')
+        oldVersion=$(git diff $(git log --format="%H" -n 2 | tail -1) HEAD "$i" | grep "^-version" | awk '{print $2}')
         IFS='-' read -ra newVersion <<< "$newVersion"
         newVersion=${newVersion//\'}
         IFS='-' read -ra oldVersion <<< "$oldVersion"

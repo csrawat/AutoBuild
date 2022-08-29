8786bbe1dd53fe66fb5a0cd8b82902994dd725fe
Chandra Shekhar Rawat <cs215100@gmail.com>
2022-08-29 14:10:17 +0530

    Create publish.sh

---
+++ publish.sh
@@ -0,0 +1,20 @@
+for i in "${modified_files=[@]}"
+do
+        echo "$i"
+        newVersion=$(git diff HEAD~1 HEAD "$i" | grep "^+version" | awk '{print $2}')
+        oldVersion=$(git diff HEAD~1 HEAD "$i" | grep "^-version" | awk '{print $2}')
+        IFS='-' read -ra newVersion <<< "$newVersion"
+        newVersion=${newVersion//\'}
+        IFS='-' read -ra oldVersion <<< "$oldVersion"
+        oldVersion=${oldVersion//\'}
+
+        echo "$newVersion"
+        echo "$oldVersion"
+
+        if [ -z "$newVersion" ]
+        then
+                continue
+        else
+                publish
+        fi
+done

c8f8745499dfc38f77ebd46b98acba756f82c484
Chandra Shekhar Rawat <cs215100@gmail.com>
2022-08-29 21:37:44 +0530

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -6,15 +6,15 @@ function publish() {
 
 echo what is this?
 
-modified_files=( $(git diff-tree --no-commit-id --name-only -r $(git log --format="%H" -n 1) | grep "$build.gradle") )
+modified_files=( $(git diff --name-only HEAD^ HEAD | grep "$build.gradle") )
 echo ${#modified_files[@]}
 echo $modified_files
 
 for i in "${modified_files=[@]}"
 do
         echo $i
-        newVersion=$(git diff $(git log --format="%H" -n 2 | tail -1) HEAD "$i" | grep "^+version" | awk '{print $2}')
-        oldVersion=$(git diff $(git log --format="%H" -n 2 | tail -1) HEAD "$i" | grep "^-version" | awk '{print $2}')
+        newVersion=$(git diff HEAD^ HEAD "$i" | grep "^+version" | awk '{print $2}')
+        oldVersion=$(git diff HEAD^ HEAD "$i" | grep "^-version" | awk '{print $2}')
         IFS='-' read -ra newVersion <<< "$newVersion"
         newVersion=${newVersion//\'}
         IFS='-' read -ra oldVersion <<< "$oldVersion"

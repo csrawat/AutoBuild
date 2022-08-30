e23f33bf1a74581955124d9846600cba00fb3829
Chandra Shekhar Rawat <cs215100@gmail.com>
2022-08-30 14:00:04 +0530

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -16,25 +16,30 @@ function publish() {
 modified_files=( $(git diff --name-only HEAD^ HEAD | grep "$build.gradle") )
 echo ${#modified_files[@]}
 
-for i in "${modified_files=[@]}"
+for each in "${modified_files[@]}"
 do
-        echo $i
-        newVersion=$(git diff HEAD^ HEAD "$i" | grep "^+version" | awk '{print $2}')
-        oldVersion=$(git diff HEAD^ HEAD "$i" | grep "^-version" | awk '{print $2}')
-        IFS='-' read -ra newVersion <<< "$newVersion"
-        newVersion=${newVersion//\'}
-        IFS='-' read -ra oldVersion <<< "$oldVersion"
-        oldVersion=${oldVersion//\'}
-
-        echo $newVersion
-        echo $oldVersion
-
-        if [ -z "$newVersion" ]
-        then
-                echo skip-continue
-                continue
-        else
-                echo build and publish
-#                 publish "$i" "$oldVersion" "$newVersion"
-        fi
+  echo "$each"
 done
+
+# for i in "${modified_files=[@]}"
+# do
+#         echo $i
+#         newVersion=$(git diff HEAD^ HEAD "$i" | grep "^+version" | awk '{print $2}')
+#         oldVersion=$(git diff HEAD^ HEAD "$i" | grep "^-version" | awk '{print $2}')
+#         IFS='-' read -ra newVersion <<< "$newVersion"
+#         newVersion=${newVersion//\'}
+#         IFS='-' read -ra oldVersion <<< "$oldVersion"
+#         oldVersion=${oldVersion//\'}
+
+#         echo $newVersion
+#         echo $oldVersion
+
+#         if [ -z "$newVersion" ]
+#         then
+#                 echo skip-continue
+#                 continue
+#         else
+#                 echo build and publish
+# #                 publish "$i" "$oldVersion" "$newVersion"
+#         fi
+# done
